7b6ec9cb117aae197ef23d5403a212875c442efc
ParkJiSu28 <qkrwltn5091@gmail.com>
2018-09-27 21:55:40 +0900

    Algo

---
+++ Alphabet_lower_upper_space_number_count.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+#include <cstdio>
+#include<string>
+/*
+int main() {
+
+	string str;
+	while (getline(cin, str)) {
+		int len = str.length();
+		int upper = 0;
+		int lower = 0;
+		int number = 0;
+		int space = 0;	
+		for (int i = 0; i < len; i++) {
+	
+			if ('a' <= str[i] && str[i] <= 'z') {
+				lower++;
+			}
+			if ('A' <= str[i] && str[i] <= 'Z') {
+				upper++;
+			}
+			if ('0' <= str[i] && str[i] <= '9') {
+				number++;
+			}
+			if (str[i] == ' ') {
+				space++;
+			}
+		}
+		cout << lower << " " << upper << " " << number << " " << space << " "<<endl;
+	}
+}*/
--- README.md
+++ README.md
@@ -9,4 +9,8 @@
 * Find Max Subarray:Divide-Conquer방식으로 배열중 최대값을 같은 서브배열의 값을 찾아본다.
  
 
-* Heapsort : recursive call 를 사용해 heapify , heapsort를 구현해본다.  
+* Heapsort : recursive call 를 사용해 heapify , heapsort를 구현해본다. 
+
+* Josephus : 조세퍼스 순열 queue 사용해서 구현
+
+* Alphabet: 대문자 소문자 공백 숫자 갯수 새기. 
